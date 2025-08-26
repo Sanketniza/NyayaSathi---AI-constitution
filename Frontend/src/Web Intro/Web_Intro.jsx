5e7f3ecf575d7e95fb2079eee5a9a96e24c93ffc
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-26 11:00:18 +0530

    Add Web_Intro component for web introduction content

---
+++ Frontend/src/Web Intro/Web_Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Web_Intro() {
+
+    return (
+
+        <>
+            <h1>Web Introduction</h1>
+            <p>This is a brief introduction to the web.</p>
+        </>
+    )
+}
+
+export default Web_Intro;
