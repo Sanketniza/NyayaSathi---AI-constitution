85cbd8fb2148b64673a3094fe2d4f185ebd08a4c
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-09 22:35:24 +0530

    remove

--- Nyayasathi Intro/src/App.jsx
+++ Nyayasathi Intro/src/App.jsx
@@ -1,37 +1,3 @@
-/* import React from 'react';
-import { ThemeProvider } from './components/ThemeProvider';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Technology from './components/Technology';
-import Dashboard from './components/Dashboard';
-import Impact from './components/Impact';
-import Stats from './components/Stats';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <ThemeProvider>
-      <div className="min-h-screen bg-primary text-text-primary transition-colors duration-300">
-        <Header />
-        <Hero />
-        <Dashboard />
-        <Features />
-        <HowItWorks />
-        <Impact />
-        <Stats />
-        <About />
-        <Contact />
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
-}
-
-export default App; */
 
 import WebIntro from "./Web Intro/WebIntro";
 
